a4ac980eb3d623f27b7c0457bf1c82f737e4bbc9
klanten87 <46435659+klanten87@users.noreply.github.com>
2019-02-09 07:34:28 +0100

    V1.02
    Egenprovning klar, ändrat lite i systembetecckningar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from openpyxl import *
+from openpyxl import load_workbook
+from openpyxl.styles import Font
 
 def systemName10(rows):       #KLAR
     system = "Not defined"
@@ -7,13 +8,9 @@ def systemName10(rows):       #KLAR
         beteckning = AS1.cell(row=i, column=2).value
         if bold.b is True:
             system =  AS1.cell(row=i, column=2).value
-            #print(system)
-        elif beteckning is None:
-            n=0
-        elif system in beteckning:
-            allreadydone = 1
-        else:
+        elif beteckning is not None and system not in beteckning:
             AS1.cell(row=i, column=2).value = system + "-" + beteckning
+
 
 
 def SkrivSkylt10(rows): #KLAR
@@ -78,28 +75,50 @@ def SkrivMotor10(rows): #KLAR
             Motor.cell(row=rad, column=5).value = F[-1]
             rad += 1
 
-def SkrivEgenprovning(c,desc,a,egen):
+def SkrivEgenprovning10(rows):  #KLAR
     rad = 4 #Startrad
-    Egen.cell(row=egen, column=1).value = c
-    Egen.cell(row=egen, column=2).value = desc
-    #Egen.cell(row=egen, column=5).value = a
+    switch =['Larm','Tidkanal','Elmätare']
+    for i in range(7,rows):
+        system = AS1.cell(row=i, column=2).font
+        if system.b is True:
+            rad += 1
+            Egen.cell(row=rad, column=1).value = AS1.cell(row=i, column=2).value
+            Egen.cell(row=rad, column=1).font = Font(bold=True)
+            rad += 1
+            continue
+        B = AS1.cell(row=i, column=2).value
+        C = AS1.cell(row=i, column=3).value
+        if B is not None and C is not None and any(x in C for x in switch):
+            E = AS1.cell(row=i, column=5).value
+            if E is None:
+                Egen.cell(row=rad, column=1).value = AS1.cell(row=i, column=2).value
+                Egen.cell(row=rad, column=2).value = AS1.cell(row=i, column=3).value
+                rad += 1
+                continue
+            Egen.cell(row=rad, column=1).value = AS1.cell(row=i, column=2).value
+            Egen.cell(row=rad, column=2).value = AS1.cell(row=i, column=5).value
+            rad += 1
+        elif B is not None:
+            Egen.cell(row=rad, column=1).value = AS1.cell(row=i, column=2).value
+            Egen.cell(row=rad, column=2).value = AS1.cell(row=i, column=3).value
+            rad += 1
 
 
-def keywordSort10(rows):
+# def keywordSort10(rows):
     #Tar nyckelordet och ser vad den ska generera fram.
     #Skickar sedan rad till Material,Bestallning, Larm och Installningsvarde
 
-def skrivBeställning10(row):
+# def skrivBeställning10(row):
     #Hämtar beställningsunderlag från rad som fås från keywordSort och skriver till Beställningslistan
     #så kollar man om det finns någon som heter lika i fabrikat och typ, summerar och sedan lägger
     # sedan den i en array som man kollar
     #det första man gör i funktionen och breakar funktionen. Annan lev sorteras bort
 
-def skrivMaterial10(row):
+# def skrivMaterial10(row):
     #
 
 
-egen = 4    #Startrad for Egenprovningen
+
 wb = load_workbook('AS1.xlsx')  #Laddar dokument
 AS1 = wb['AS1']                 #Laddar flik AS1
 Skyltlista = wb['Skyltlista']   #Laddar flik Skyltlista
@@ -107,49 +126,11 @@ Motor = wb['Provning motorer']  #Laddar flik Provning motorer
 Egen = wb['Egenkontroll']       #Laddar flik Egenkontroll
 
 rows = AS1.max_row              #Kollar vilken sista raden ar
-#rows = 40
 
 systemName10(rows)
 SkrivSkylt10(rows)
 SkrivMotor10(rows)
-# for i in range(7,rows):
-    # t = AS1.cell(row=i, column=1).value
-    # m = AS1.cell(row=i, column=6).value
-    # b = AS1.cell(row=i, column=2).font
-    # if b.b is True:
-    #     egen +=1
-    # for y in range(8,14):
-    #     e = AS1.cell(row=i, column=y).value
-    #     if e is None:
-    #         n = 0
-    #     else:
-    #         c = AS1.cell(row=i, column=2).value
-    #         desc = AS1.cell(row=i, column=3).value
-    #         a = AS1.cell(row=5, column=2).value
-    #         SkrivEgenprovning(c,desc,a,egen)
-    #         egen += 1
-    #         break
-    #
-    # if t is None:
-    #     #print("None at row "+str(i))
-    #     n = 0
-    # elif "s" in t:
-    #     c = AS1.cell(row=i, column=2).value
-    #     desc = AS1.cell(row=i, column=3).value
-    #     a = AS1.cell(row=5, column=2).value
-    #     SkrivSkylt(c,desc,a,skylt)
-    #     skylt += 1
-    #
-    # if m is None:
-    #     # print("None at row "+str(i))
-    #     n = 0
-    # elif "@" in m:
-    #     c = AS1.cell(row=i, column=2).value
-    #     desc = AS1.cell(row=i, column=3).value
-    #     a = AS1.cell(row=i, column=6).value
-    #     SkrivMotor(c, desc, a, motor)
-    #     motor += 1
-
+SkrivEgenprovning10(rows)
 
 
 wb.save('AS1_genererad.xlsx')
